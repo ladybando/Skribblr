e8e1a58d1eb92c3689da309aa4211f5f5d4ba873
Ayana Bando <ayanabando@gmail.com>
2021-08-27 20:27:56 -0400

    Delete Users.kt
    remove to rebuild app

--- app/src/main/java/com/example/android/skribblr/model/Users.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android.skribblr.model
-
-import androidx.room.Entity;
-import androidx.room.PrimaryKey
-
-class Users(@PrimaryKey val id: Long, var userID: Int, var userName: String) {
-}
