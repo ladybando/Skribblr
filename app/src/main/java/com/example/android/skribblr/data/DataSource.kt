0458de566d0b702ec731ce12116b29e89e7a9076
Ayana Bando <ayanabando@gmail.com>
2021-08-28 11:16:31 -0400

    Update DataSource.kt
    update spelling of skribbl

--- app/src/main/java/com/example/android/skribblr/data/DataSource.kt
+++ app/src/main/java/com/example/android/skribblr/data/DataSource.kt
@@ -1,27 +1,53 @@
 package com.example.android.skribblr.data
 
 import android.content.res.Resources
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class DataSource(resources: Resources) {
-    private val initialSkribbleList = skribblesList(resources)
-    private val skribbleLiveData = MutableLiveData(initialSkribbleList)
-    val currentList = skribbleLiveData.value
+    private val initialSkribblList = skribblsList(resources)
+    private val skribblLiveData = MutableLiveData(initialSkribblList)
+    val currentList = skribblLiveData.value
 
     //adds new note to list
-    fun addSkribble(skribble: Skribble){
+    fun addSkribble(skribbl: Skribbl){
         if (currentList == null){
-            skribbleLiveData.postValue(listOf(skribble))
+            skribblLiveData.postValue(listOf(skribbl))
         }else{
             val updatedList=currentList.toMutableList()
-            updatedList.add(0,skribble)
-            skribbleLiveData.postValue(updatedList)
+            updatedList.add(0,skribbl)
+            skribblLiveData.postValue(updatedList)
         }
     }
 
-    fun removeSkribble(skribble: Skribble){
+    fun removeSkribble(skribbl: Skribbl){
         if (currentList != null){
-
+            val updatedList=currentList.toMutableList()
+            updatedList.remove(skribbl)
+            skribblLiveData.postValue(updatedList)
         }
     }
+
+    fun getSkribblByID(id:Long): Skribbl?{
+        skribblLiveData.value?.let{ skribbl->
+            return skribbl.firstOrNull{(it.id == id)}
+        }
+        return null
+    }
+
+    fun getSkribblList():LiveData<List<Skribbl>>{
+        return skribblLiveData
+    }
+    companion object {
+        private var INSTANCE: DataSource? = null
+
+        fun getDataSource(resources: Resources): DataSource {
+            return synchronized(DataSource::class) {
+                val newInstance = INSTANCE ?: DataSource(resources)
+                INSTANCE = newInstance
+                newInstance
+            }
+        }
+    }
+
 }
