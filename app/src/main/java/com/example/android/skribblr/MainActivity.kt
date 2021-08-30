49fd483adb1add24792038539215fb8967795134
Ayana Bando <ayanabando@gmail.com>
2021-08-30 10:26:30 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/android/skribblr/MainActivity.kt
+++ app/src/main/java/com/example/android/skribblr/MainActivity.kt
@@ -1,26 +1,50 @@
 package com.example.android.skribblr
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.android.skribblr.adapter.ItemAdapter
-import com.example.android.skribblr.data.Datasource
+import android.widget.EditText
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.android.stickies.adapter.SkribblAdapter
+import com.example.android.stickies.data.DataSource
+import com.example.android.stickies.data.Skribbl
+import com.example.android.stickies.databinding.ActivityListBinding
+import com.example.android.stickies.databinding.ActivityMainBinding
+import com.example.android.stickies.databinding.AddNewActivityBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val listBinding = ActivityListBinding.inflate(layoutInflater)
+        val mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val addNewActivityBinding = AddNewActivityBinding.inflate(layoutInflater)
 
-        // Initialize data.
-        val myDataset = Datasource().loadSkribbls()
+        setContentView(mainBinding.root)
+        //load data
+        val resources = resources
+        val myDataset = DataSource().skribblsList(resources)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = ItemAdapter(this, myDataset)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        // Use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true)
+        //access views
+        val recyclerView = mainBinding.recyclerView
+        recyclerView.adapter = SkribblAdapter(this, myDataset)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        val adapter=SkribblAdapter(this,myDataset)
+
+        mainBinding.fab.setOnClickListener {
+            val inputSkribbl = addNewActivityBinding.addNewSkribblField.text.toString()
+            myDataset.add(Skribbl("", Date(), inputSkribbl, "HIGH"))
+            adapter.notifyItemInserted(0)
+        }
+
+        mainBinding.editNewSkribbl.setOnClickListener {
+            val inputSkribbl = mainBinding.editNewSkribbl.text.toString()
+            
+            mainBinding.submitButton.setOnClickListener {
+                myDataset.add(Skribbl("", Date(), inputSkribbl, "MEDIUM"))
+                adapter.notifyItemInserted(0)
+            }
+        }
     }
-
 }
