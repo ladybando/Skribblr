861bf79b17cdb65fae8a2e91c51aeea5aab2c1ae
Ayana Bando <ayanabando@gmail.com>
2021-08-30 10:27:53 -0400

    Update DataSource.kt

--- app/src/main/java/com/example/android/skribblr/data/DataSource.kt
+++ app/src/main/java/com/example/android/skribblr/data/DataSource.kt
@@ -4,50 +4,43 @@ import android.content.res.Resources
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
-class DataSource(resources: Resources) {
-    private val initialSkribblList = skribblsList(resources)
-    private val skribblLiveData = MutableLiveData(initialSkribblList)
-    val currentList = skribblLiveData.value
+import android.content.res.Resources
+import com.example.android.stickies.R
+import java.util.*
 
-    //adds new note to list
-    fun addSkribble(skribbl: Skribbl){
-        if (currentList == null){
-            skribblLiveData.postValue(listOf(skribbl))
-        }else{
-            val updatedList=currentList.toMutableList()
-            updatedList.add(0,skribbl)
-            skribblLiveData.postValue(updatedList)
-        }
+class DataSource{
+    fun skribblsList(resources : Resources):  MutableList<Skribbl> {
+        return mutableListOf(
+            Skribbl(
+                title = "",
+                date = Date(8 / 28 / 2020.toLong()),
+                note = resources.getString(R.string.skribbl1),
+                urgency = "Low"
+            ),
+            Skribbl(
+                title = "",
+                date = Date(8 / 28 / 2020.toLong()),
+                note = resources.getString(R.string.skribbl2),
+                urgency = "Medium"
+            ),
+            Skribbl(
+                title = "",
+                date = Date(8 / 28 / 2020.toLong()),
+                note = resources.getString(R.string.skribbl3),
+                urgency = "Low"
+            ),
+            Skribbl(
+                title = "hate laundry",
+                date = Date(8 / 28 / 2020.toLong()),
+                note = resources.getString(R.string.skribbl4),
+                urgency = "Low"
+            ),
+            Skribbl(
+                title = "",
+                date = Date(8 / 28 / 2020.toLong()),
+                note = resources.getString(R.string.skribbl5),
+                urgency = "Low"
+            )
+        )
     }
 
-    fun removeSkribble(skribbl: Skribbl){
-        if (currentList != null){
-            val updatedList=currentList.toMutableList()
-            updatedList.remove(skribbl)
-            skribblLiveData.postValue(updatedList)
-        }
-    }
-
-    fun getSkribblByID(id:Long): Skribbl?{
-        skribblLiveData.value?.let{ skribbl->
-            return skribbl.firstOrNull{(it.id == id)}
-        }
-        return null
-    }
-
-    fun getSkribblList():LiveData<List<Skribbl>>{
-        return skribblLiveData
-    }
-    companion object {
-        private var INSTANCE: DataSource? = null
-
-        fun getDataSource(resources: Resources): DataSource {
-            return synchronized(DataSource::class) {
-                val newInstance = INSTANCE ?: DataSource(resources)
-                INSTANCE = newInstance
-                newInstance
-            }
-        }
-    }
-
-}
