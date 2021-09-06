016b50b5245112103e9b7b037a59cfb52665311b
Ayana Bando <ayanabando@gmail.com>
2021-09-06 09:08:01 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/android/skribblr/MainActivity.kt
+++ app/src/main/java/com/example/android/skribblr/MainActivity.kt
@@ -1,50 +1,30 @@
 package com.example.android.skribblr
 
-import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import androidx.recyclerview.widget.GridLayoutManager
-import com.example.android.stickies.adapter.SkribblAdapter
-import com.example.android.stickies.data.DataSource
-import com.example.android.stickies.data.Skribbl
-import com.example.android.stickies.databinding.ActivityListBinding
-import com.example.android.stickies.databinding.ActivityMainBinding
-import com.example.android.stickies.databinding.AddNewActivityBinding
-import java.util.*
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.android.skribblr.adapter.RecyclerViewAdapter
+import com.example.android.skribblr.data.DataSource
+import com.example.android.skribblr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val listBinding = ActivityListBinding.inflate(layoutInflater)
-        val mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        val addNewActivityBinding = AddNewActivityBinding.inflate(layoutInflater)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(mainBinding.root)
-        //load data
-        val resources = resources
-        val myDataset = DataSource().skribblsList(resources)
+        val recyclerView=binding.recyclerView
+        recyclerView.layoutManager=StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
-        //access views
-        val recyclerView = mainBinding.recyclerView
-        recyclerView.adapter = SkribblAdapter(this, myDataset)
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        val note = "Wash hands"
+        val list=DataSource(note).skribblList()
+        val adapter=RecyclerViewAdapter(this,list)
+        recyclerView.adapter=adapter
 
-        val adapter=SkribblAdapter(this,myDataset)
-
-        mainBinding.fab.setOnClickListener {
-            val inputSkribbl = addNewActivityBinding.addNewSkribblField.text.toString()
-            myDataset.add(Skribbl("", Date(), inputSkribbl, "HIGH"))
-            adapter.notifyItemInserted(0)
-        }
-
-        mainBinding.editNewSkribbl.setOnClickListener {
-            val inputSkribbl = mainBinding.editNewSkribbl.text.toString()
-            
-            mainBinding.submitButton.setOnClickListener {
-                myDataset.add(Skribbl("", Date(), inputSkribbl, "MEDIUM"))
-                adapter.notifyItemInserted(0)
-            }
+        //TODO set floating action button to move to NewSkribblFragment
+        //create onClick method and call in onClickListener lambda
+        binding.fab.setOnClickListener {
+            setContentView(R.layout.fragment_new_skribbl)
         }
     }
 }
