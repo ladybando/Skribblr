5980c4cb4ac9ea6f8a99fa7eebf7996ca6aaff9d
Ayana Bando <ayanabando@gmail.com>
2021-09-06 09:06:32 -0400

    Update DataSource.kt

--- app/src/main/java/com/example/android/skribblr/data/DataSource.kt
+++ app/src/main/java/com/example/android/skribblr/data/DataSource.kt
@@ -2,8 +2,6 @@ package com.example.android.skribblr.data
 
 data class DataSource(var notes: String)  {
 //takes in user input as a string
-
-    //should function also accept a string as an argument? note?
    fun skribblList(): MutableList<String>{
         val tempList=mutableListOf("Wash dishes", "Clean clothes", "Wash car", "Finish project")
        return tempList
